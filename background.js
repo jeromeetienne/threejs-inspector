d97a8c6d018f9839f870382d7bcc04fd2a2f4745
Jerome Etienne <jerome.etienne@gmail.com>
2016-01-08 17:27:11 +0000

    more work

--- background.js
+++ background.js
@@ -15,23 +15,23 @@ chrome.runtime.onConnect.addListener(function(devToolsConnection) {
                         console.log('in background.js: trying to inject', message.data.file , 'in', message.data.tabId)
                         console.log('in background.js: message.data', message.data)
                         // Inject a content script into the identified tab
-                        chrome.tabs.executeScript( message.data.tabId, {
+                        chrome.tabs.executeScript(message.data.tabId, {
                                 file: message.data.file 
-                        });                        
+                        });
                 }else{
                         console.assert(false, 'in background.js: unknown message type', message.type)
                 }
         }
         // add the listener
         devToolsConnection.onMessage.addListener(onMessage);
-        
+
         // remove the listener
         devToolsConnection.onDisconnect.addListener(function() {
                 console.log('in background.js: onDisconnect from devtools page', devToolsConnection)
                 devToolsConnection.onMessage.removeListener(onMessage);
                 devToolsConnection = null
         });
-        
+
         // setTimeout(function(){
         //         console.log('in background.js: notify devTools that background page is connected')
         //         // notify devtools page that it is connected
