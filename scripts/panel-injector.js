ce5c01d68763f0cfbb9867783c039f6f65706158
Jerome Etienne <jerome.etienne@gmail.com>
2016-01-12 20:51:16 +0000

    more work

--- scripts/panel-injector.js
+++ scripts/panel-injector.js
@@ -14,6 +14,14 @@ PanelWin3js.plainFunction	= function(fct, args){
 				+ JSON.stringify(args)
 			+ ')'
 
+	PanelWin3js.evalJsCode(jsCode)
+}
+
+/**
+ * inject a function and run it on a object3d
+ * @param  {Function} fct  - the function to call
+ */
+PanelWin3js.evalJsCode	= function(jsCode){
 	chrome.devtools.inspectedWindow.eval( jsCode, function(result, isException){
 		if( isException ){
 			console.error('Exception while eval()', jsCode)
